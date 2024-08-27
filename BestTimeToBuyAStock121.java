57c43c929efd74408da0aa5dd2fe9e6bbeb6ea9e
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-08-27 12:30:37 +0530

    KadanesAlgo

---
+++ BestTimeToBuyAStock121.java
@@ -0,0 +1,36 @@
+public class BestTimeToBuyAStock121 {
+  public static void main(String[] args) {
+    int arr[]={4,5,6,7,1,9,8,11};
+        //System.out.println(maxProfitBrt(arr));
+    System.out.println(maxProfit(arr));
+  }
+  //kadanes algo
+private static int maxProfit(int[] arr) {
+  int buy=arr[0],profit=0;
+  for (int i = 0; i < arr.length; i++) {
+    buy=Math.min(arr[i], buy);
+    profit=Math.max(profit, arr[i]-buy);
+  }
+
+  return profit;
+}
+
+  //brute force 
+  private static int maxProfitBrt(int[] arr) {
+    int profit=0,max=0;
+      for(int i=0;i<arr.length-1;i++)
+      {
+        for(int j=i+1;j<arr.length;j++)
+        {
+            if(arr[i]<arr[j])
+            {
+              profit=arr[j]-arr[i];
+              max=Math.max(max, profit);
+            }
+        }
+      }
+    return max;
+    }
+
+
+}
--- KadanesAlgo.java
+++ KadanesAlgo.java
@@ -3,8 +3,9 @@
 public class KadanesAlgo {
   public static void main(String[] args) {
     int [] arr={-2,1,-3,4,-1,2,1,-5,4};
-    
     System.out.println(maxSubArray(arr));
+
+  
   }
 
   private static int maxSubArray(int[] arr) {
