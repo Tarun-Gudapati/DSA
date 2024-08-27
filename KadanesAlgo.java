2e755b9cad2143dbfa2b3ebfe5c4c6cb19dfdd97
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-08-27 11:39:39 +0530

    problem number 53 MAximum subarray

---
+++ KadanesAlgo.java
@@ -0,0 +1,21 @@
+//problem number 53 MAximum subarray
+
+public class KadanesAlgo {
+  public static void main(String[] args) {
+    int [] arr={-2,1,-3,4,-1,2,1,-5,4};
+    
+    System.out.println(maxSubArray(arr));
+  }
+
+  private static int maxSubArray(int[] arr) {
+
+    int sum=Integer.MIN_VALUE,count=arr[0];
+    for(int i=1;i<arr.length;i++)
+    { 
+      count =Math.max(arr[i], count+arr[i]);
+      sum=Math.max(count, sum);
+    }
+    return sum;
+  }
+  
+}
--- Problem136SingleNumber.java
+++ Problem136SingleNumber.java
@@ -1,8 +1,12 @@
 public class Problem136SingleNumber {
 public static void main(String[] args) {
-  int  arr[] ={10,12,4,10,12,4,5};
-  int a=singleNumber(arr);
-  System.out.println(a);
+ int  arr[] ={0,2,1,1,0,2,1};
+  // int a=singleNumber(arr);
+  // System.out.println(a);
+  sortColors(arr);
+  for (int i : arr) {
+    System.out.print(i+" ");
+  }
 }
     public static int singleNumber(int[] nums) {
         int size=0,value=0;
@@ -12,4 +16,40 @@ public static void main(String[] args) {
         }
         return value;
     }
+  
+      public static void sortColors(int[] nums) {
+          int z=0,One=0,Two=0;
+          for(int i=0;i<nums.length;i++)
+          {
+              if(nums[i]==0)
+              {
+                  z++;
+              }
+              else if(nums[i]==1)
+              {
+                  One++;
+              }
+              else{
+                  Two++;
+              }
+  
+          }
+          for(int i=0;i<nums.length;i++)
+          {
+             if(i<z)
+             {
+              nums[i]=0;
+             }
+               else if(i>=z)
+             {
+              nums[i]=1;
+             }
+              if(i>z+Two)
+             {
+              nums[i]=2;
+             }
+  
+          }
+      }
+  
 }
