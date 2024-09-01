dccf510bd5feefea0229d6435d27feca397732c0
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-09-01 22:01:14 +0530

    using Recursion

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -7,14 +7,41 @@ public class BinarySearch {
     int [] arr ={1,5,6,7,8,9,77};
     Arrays.sort(arr);
     int target=77;
-    System.out.println( binarySearch(arr,target));
+    int low=0 ;
+int high=arr.length-1;
+    
+
+System.out.println( binarySearch(arr,target));
+    System.out.println( recBinarySearch(arr,target,low,high));
+  
   }
 
-  private static int binarySearch(int[] arr, int target) {
-   
+  private static int recBinarySearch(int[] arr, int target, int low, int high) {
+  
+  if(high<low)
+  {
+    return -1;
+  }
+    int mid =(low+high)/2;
+
+  if(arr[mid]==target)
+  return mid; 
+  if(arr[mid]>target)
+  {
+    high=mid-1;
+    return recBinarySearch(arr, target, low, high);
+  }
+  else
+  {
+  low=mid+1;
+  return recBinarySearch(arr, target, low, high);
+  }
+  
+  }
+
+  private static int binarySearch(int[] arr, int target) {  
 int low=0 ;
 int high=arr.length-1;
-
 int mid;
 while(high>=low)
 {
