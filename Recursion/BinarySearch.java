cef4b0aebf6a181e7c2d9f4e81d54a1aabbf018d
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-09-01 20:58:44 +0530

    binary search

---
+++ Recursion/BinarySearch.java
@@ -0,0 +1,43 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+  public static void main(String[] args) {
+    int [] arr ={1,5,6,7,8,9,77};
+    Arrays.sort(arr);
+    int target=77;
+    System.out.println( binarySearch(arr,target));
+  }
+
+  private static int binarySearch(int[] arr, int target) {
+    int finalV=arr[0];
+int low=0 ;
+int high=arr.length-1;
+
+int mid;
+while(high!=low)
+{
+    mid=(high+low)/2;
+    if (arr[mid]==target) {
+      return mid;
+      
+    }
+    else{
+      if(arr[mid]>target)
+      {
+        high=mid-1;
+      }
+      else
+      {
+        low=mid+1;
+      }
+    }
+}
+if (high==low) {
+  return low;
+}
+   
+   return finalV;
+  }
+}
