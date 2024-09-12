d260ec494cee8122510bfd95208a1b812bd861e4
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-09-12 21:38:52 +0530

    new prog

--- obj/Debug/net8.0/DSA.AssemblyInfo.cs
+++ obj/Debug/net8.0/DSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ff2bd7ab887a44be62e196a7b822ab2c003992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61417f7e02e908c358115a2164b1bb80b6c992a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
