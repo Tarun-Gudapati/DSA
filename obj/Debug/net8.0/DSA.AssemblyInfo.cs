c8effbc26f9407105eb8ab202a97267c67e8143c
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-09-15 21:11:55 +0530

    CapacityShip java

--- obj/Debug/net8.0/DSA.AssemblyInfo.cs
+++ obj/Debug/net8.0/DSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d70d64e0112993f9b7e0bd856998a314e43aaac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56b6154b19311ebb715948fb82b2c5716f2f0d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
