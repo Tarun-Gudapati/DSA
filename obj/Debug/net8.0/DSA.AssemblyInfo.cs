4557a05dbb9079a7a70a7b14c3f8c55406f66053
Tarun-Gudapati <tarungudapati539@gmail.com>
2024-11-07 23:09:42 +0530

    FCGVHBJNKM

--- obj/Debug/net8.0/DSA.AssemblyInfo.cs
+++ obj/Debug/net8.0/DSA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ceeab9584425e3bf2993c0248f8206c7e9f6fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1a6c155e886c84da7dfbe221b4ce591c618639")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
